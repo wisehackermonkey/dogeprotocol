11dcd9b72f7618e4cd5bbaba3d84562bb95a2601
Oran C <oranbusiness@gmail.com>
2023-09-13 00:32:46 -0700

    fixed decoding!

--- main.py
+++ main.py
@@ -10,27 +10,12 @@ class Doge(Packet):
         ByteField("version",1),
         FieldLenField("toptext_len", None, count_of="toptext"),
         StrField("toptext",None, fmt="H"),
-        
-        FieldLenField("bottomtext_len", None, count_of="bottomtext"),
+        StrStopField("toptext_end",default="\x09",stop="\x09"),
 
         StrField("bottomtext",None, fmt="H"),
+        StrStopField("bottomtext_end",default="\x09",stop="\x09"),
+
         ByteEnumField("gotum", 2, {1:"uhh",2:"deez-nuts"}),
         StrFixedLenField("doge_png",34332),
         XShortField("do_you_get_it",None)#aka checksum
     ]
-    def addfield(self, pkt, s, val):
-        return s+self.i2m(pkt, val)
-    def do_dissect(self, s):
-        flist = self.fields_desc[:]
-        flist.reverse()
-        while s and flist:
-            f = flist.pop()
-            s,fval = f.getfield(self, s)
-            self.fields[f] = fval
-        return s
-class StrFixedLenField(StrField):
-    def addfield(self, pkt, s, val):
-        return s+struct.pack("%is"%self.length,self.i2m(pkt, val))
-class StrFixedLenField(StrField):
-    def getfield(self, pkt, s):
-        return s[self.length:], self.m2i(pkt,s[:self.length])
